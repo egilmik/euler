47e5d4a8ee661b2a1bb6d61d42f9738f5e28e078
Egil Mikalsen <egilmikalsen@gmail.com>
2015-02-02 19:23:26 +0100

    Finished with solution

--- src/problems/11/11.go
+++ src/problems/11/11.go
@@ -7,41 +7,85 @@ import "bufio"
 import "strings"
 import "strconv"
 
-var numbers = 4
+func main() {
+	grid := getGrid()
+	var sum uint64
+	var largest uint64
+	sum = 0
+	largest = 0
+	for y := 0; y < 20; y++ {
+		for x := 0; x < 20; x++ {
 
-func main(){
-  grid := getGrid()
-  fmt.Println(calulateLargestForIndex(grid,0,0))
+			sum = calulateLargestForIndex(grid, x, y)
+			if sum > largest {
+				largest = sum
+			}
+		}
+
+	}
+	fmt.Println(largest)
 }
 
 func calulateLargestForIndex(grid [][]uint64, x int, y int) uint64 {
-  if len(grid) > y {
-    line := grid[y]
-    return line[x]
+	var largest uint64
+	largest = 0
+	var value uint64
 
-  }
-  return 0
+	down := len(grid) > y+3
+	right := len(grid[y]) > x+3
+	up := y > 2
+	//Right
+	if right {
+		value = grid[y][x] * grid[y][x+1] * grid[y][x+2] * grid[y][x+3]
+		if value > largest {
+			largest = value
+		}
+	}
+
+	//Down
+	if down {
+		value = grid[y][x] * grid[y+1][x] * grid[y+2][x] * grid[y+3][x]
+		if value > largest {
+			largest = value
+		}
+	}
+
+	if right && down {
+		value = grid[y][x] * grid[y+1][x+1] * grid[y+2][x+2] * grid[y+3][x+3]
+		if value > largest {
+			largest = value
+		}
+	}
+
+	if up && right {
+		value = grid[y][x] * grid[y-1][x+1] * grid[y-2][x+2] * grid[y-3][x+3]
+		if value > largest {
+			largest = value
+		}
+	}
+
+	return largest
 }
 
 func getGrid() [][]uint64 {
-  file, err := os.Open("grid.txt")
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer file.Close()
+	file, err := os.Open("grid.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-  grid := make([][]uint64,0)
+	var grid [][]uint64
 
-  scanner := bufio.NewScanner(file)
-  for scanner.Scan(){
-    var row []uint64
-    line := scanner.Text()
-    splits := strings.Split(line," ")
-    for _, element := range splits{
-      val, _ :=strconv.ParseUint(element,10,8)
-      row = append(row,val)
-    }
-    grid = append(grid,row)
-  }
-  return grid
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		var row []uint64
+		line := scanner.Text()
+		splits := strings.Split(line, " ")
+		for _, element := range splits {
+			val, _ := strconv.ParseUint(element, 10, 8)
+			row = append(row, val)
+		}
+		grid = append(grid, row)
+	}
+	return grid
 }
